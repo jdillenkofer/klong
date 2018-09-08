bb8005e3b2576f8c69b7cd8d48e395d9ea493ef1
Jan Dillenkofer <Jan.Dillenkofer@t-online.de>
2018-09-08 23:36:51 +0200

    basic interpreter without function support

---
+++ src/terp/environment.h
@@ -0,0 +1,8 @@
+//
+// Created by jdillenkofer on 08.09.18.
+//
+
+#ifndef KLONG_ENVIRONMENT_H
+#define KLONG_ENVIRONMENT_H
+
+#endif //KLONG_ENVIRONMENT_H
---
+++ src/terp/treewalk_terp.h
@@ -0,0 +1,8 @@
+//
+// Created by jdillenkofer on 08.09.18.
+//
+
+#ifndef KLONG_TREEWALK_TERP_H
+#define KLONG_TREEWALK_TERP_H
+
+#endif //KLONG_TREEWALK_TERP_H
