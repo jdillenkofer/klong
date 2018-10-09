0514ae0d637d9e5cc09edc224b4523b6df43c1bd
Jan Dillenkofer <Jan.Dillenkofer@t-online.de>
2018-10-09 13:02:22 +0200

    enable caching travis

--- get-deps.sh
+++ get-deps.sh
@@ -2,9 +2,13 @@
 
 mkdir -p deps
 cd deps
-wget http://releases.llvm.org/7.0.0/llvm-7.0.0.src.tar.xz
-tar -xf llvm-7.0.0.src.tar.xz
-cd llvm-7.0.0.src
+if [ ! -f "llvm-7.0.0.src.tar.xz" ]; then
+    wget "http://releases.llvm.org/7.0.0/llvm-7.0.0.src.tar.xz"
+fi
+if [ ! -d "llvm-7.0.0.src" ]; then
+    tar -xf "llvm-7.0.0.src.tar.xz"
+fi
+cd "llvm-7.0.0.src"
 mkdir -p build
 cd build
 cmake -DCMAKE_BUILD_TYPE=Debug ..
