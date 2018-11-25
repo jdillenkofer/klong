a35ea20d717bf36e59e4d486f238f4947081eca3
Jan Dillenkofer <Jan.Dillenkofer@t-online.de>
2018-11-25 19:59:50 +0100

    Fix windows tests

--- test/run.py
+++ test/run.py
@@ -49,9 +49,9 @@ def link(objfile, executable):
             "/DEFAULTLIB:libcmt", 
             objfile, 
             "/OUT:" + executable,
-            "/LIBPATH:C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.15.26726/lib/x64",
-            "/LIBPATH:C:/Program Files (x86)/Windows Kits/10/Lib/10.0.17134.0/um/x64", 
-            "/LIBPATH:C:/Program Files (x86)/Windows Kits/10/Lib/10.0.17134.0/ucrt/x64"
+            "/LIBPATH:C:/Program Files (x86)/Microsoft Visual Studio/2017/Community/VC/Tools/MSVC/14.16.27023/lib/x64",
+            "/LIBPATH:C:/Program Files (x86)/Windows Kits/10/Lib/10.0.17763.0/um/x64", 
+            "/LIBPATH:C:/Program Files (x86)/Windows Kits/10/Lib/10.0.17763.0/ucrt/x64"
         ], stdout=subprocess.PIPE)
     else:
         result = subprocess.call([
